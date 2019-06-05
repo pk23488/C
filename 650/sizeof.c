48ab1e3d1db490db959108ce546528ed707f68bd
pk23488 <947612133@qq.com>
2019-06-05 11:05:07 +0800

    "数据类型（补）"

---
+++ 650/sizeof.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main()
+{
+	printf("%d\n",sizeof(int));
+	printf("%d\n",sizeof(double));
+	printf("%d\n",sizeof(short));
+	printf("%d\n",sizeof(float));
+	printf("%d\n",sizeof(unsigned int));
+	printf("%d\n",sizeof(long));
+}
